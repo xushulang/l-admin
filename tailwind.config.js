6ac41498b53708569d3e54b72d3d371fd1a89a0c
xushulang <xushulang@msn.cn>
2024-05-24 17:03:03 +0800

    配置tailwindcss darkMode

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'selector',
+
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
