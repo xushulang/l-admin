7ad4f1db5413c9a20b268746cd3f281df06e603a
xushulang <xushulang@msn.cn>
2024-05-25 13:41:11 +0800

    手机号验证Rule

---
+++ app/Rules/Phone.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class Phone implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (!preg_match('/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/', $value)) {
+            $fail('validation.phone')->translate();
+        }
+    }
+}
