64762b13fc8b361296cbb2642a63e429f132ed59
xushulang <xushulang@msn.cn>
2024-09-17 16:52:11 +0800

    js基础文件更新

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,35 +1,38 @@
-import './bootstrap';
-import '../css/app.css';
+import type { DefineComponent } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import { i18nVue } from 'laravel-vue-i18n'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import { createApp, h } from 'vue'
+import { ZiggyVue } from '../../vendor/tightenco/ziggy'
 
-import { createApp, h, DefineComponent } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createPinia } from 'pinia';
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import { i18nVue } from 'laravel-vue-i18n';
+import './bootstrap'
+import '../css/app.css'
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-const pinia = createPinia();
-pinia.use(piniaPluginPersistedstate);
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel'
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
+    title: title => `${title} - ${appName}`,
+    resolve: name =>
+        resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(pinia)
             .use(ZiggyVue)
             .use(i18nVue, {
+                lang: import.meta.env.VITE_APP_LOCALE,
                 resolve: async (lang: string) => {
-                    const langs = import.meta.glob('../../lang/*.json', { eager: true });
-                    return await langs[`../../lang/${lang}.json`];
+                    const langs = import.meta.glob('../../lang/*.json')
+                    return await langs[`../../lang/${lang}.json`]()
                 },
             })
-            .mount(el);
+            .mount(el)
     },
     progress: {
         color: '#4B5563',
     },
-});
+})
--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,13 +1,10 @@
-import axios from 'axios';
-window.axios = axios;
+import axios from 'axios'
+import Echo from 'laravel-echo'
+import Pusher from 'pusher-js'
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-import Echo from 'laravel-echo';
-
-import Pusher from 'pusher-js';
-window.Pusher = Pusher;
-
+window.axios = axios
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+window.Pusher = Pusher
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
@@ -17,4 +14,4 @@ window.Echo = new Echo({
     wssPort: import.meta.env.VITE_PUSHER_PORT,
     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
-});
+})
