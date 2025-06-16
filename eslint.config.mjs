2cf4ef9a96ab7859ce8242c35fc71adba2a27c11
xushulang <xushulang@msn.cn>
2025-06-16 13:47:05 +0000

    更新eslint配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,14 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-    ignores: ['**/vendor/', 'public/build/'],
+    ignores: ['**/vendor/', 'public/build/', 'meilisearch_data/', 'mysql_data/', 'node_modules/', 'storage/'],
     formatters: true,
     stylistic: {
         indent: 4,
     },
     yaml: {
         overrides: {
-            'yaml/indent': 2,
+            'yaml/indent': ['error', 4, { indentBlockSequences: true, indicatorValueIndent: 2 }],
         },
     },
 })
